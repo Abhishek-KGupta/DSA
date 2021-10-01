52cbef8b3bf5581a2e15ed0e5fe9d71d9aa8806b
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2021-10-01 22:00:35 +0530

    Add files via upload

---
+++ treerecursion.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void tail(int n)
+{
+    if(n>0)
+    {
+        cout<<n;
+        tail(n-1);
+        tail(n-1);
+    }
+}
+int main()
+{
+    tail(4);
+    return 0;
+}
