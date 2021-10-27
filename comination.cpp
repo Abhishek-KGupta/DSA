3b82eacf9483586914ee706be74e7bfeb38009f9
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2021-10-27 23:56:30 +0530

    Add files via upload

---
+++ comination.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int C(int n,int r)
+{
+    if(r==0||n==r)
+        return 1;
+    else
+        return C(n-1,r-1)+C(n-1,r);
+}
+int main()
+{
+    int n,r;
+    cin>>n>>r;
+    cout<<C(n,r);
+}
