ab1e8e29102aca14ac3b19a8aad3a60c86cab1e5
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2022-06-24 16:42:25 +0530

    Time: 49 ms (19.04%), Space: 26.3 MB (6.93%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,31 +3,47 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         long long int tot=1;
         int z=0;
-        for(auto x:nums)
-        {
-            if(x!=0)
-                tot*=x;
-            if(x==0)
-                z++;
-        }
-        
-        vector<int> m(nums.size(),0);
-        if(z>1)
-            return m;
+        vector<long long int> pre(nums.size(),1);
+        vector<long long int> post(nums.size(),1);
         
         for(int i=0;i<nums.size();i++)
         {
-            if(z==0)
-                nums[i]=tot/nums[i];
-            
-            else if(z==1)
+            if(i==0)
             {
-                if(nums[i]==0)
-                    nums[i]=tot;
-                else
-                    nums[i]=0;
+                pre[i]=nums[i];
+                post[nums.size()-1]=nums[nums.size()-1];
+            }
+            else if(i==nums.size()-1)
+            {
+                pre[i]=pre[i-1]*nums[i];
+                post[0]=post[1]*nums[0];
+            }
+            else 
+            {
+                pre[i]=pre[i-1]*nums[i];
+                post[nums.size()-1-i]=post[nums.size()-i]*nums[nums.size()-1-i ];
             }
         }
+        // for(auto x:pre)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        // for(auto y:post)
+        //     cout<<y<<" ";
+        // cout<<endl;
+        // vector<int> m(nums.size(),0);
+        // if(z>1)
+        //     return m;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i==0)
+                nums[0]=post[1];
+            else if(i==nums.size()-1)
+                nums[i]=pre[i-1];
+            else
+                nums[i]=pre[i-1]*post[i+1];
+        }
+            
         return nums;
 
     }
