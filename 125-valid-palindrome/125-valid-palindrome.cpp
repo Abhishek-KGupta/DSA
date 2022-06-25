6e2a58efd0ffb25cfeeba4ed38fbb3e9f6d15cdc
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2022-06-25 12:05:55 +0530

    Time: 7 ms (66.79%), Space: 7.3 MB (75.82%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -2,24 +2,28 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int start=0, end=s.length()-1;
-	while(start<end) {
-		if (!isalnum(s[start])) start++;
-		else if (!isalnum(s[end])) end--;
-		else {
-			if (tolower(s[start++])!=tolower(s[end--])) return false;
-		}
-	}
-	return true;
+        int front=0, back=s.size()-1;
+        while(front<=back)
+        {
+            if(!((s[front]>='a' and s[front]<='z') or (s[front]>='A' and s[front]<='Z') or (s[front]>='0' and s[front]<='9')))
+                front++;
+            if(!((s[back]>='a' and s[back]<='z') or (s[back]>='A' and s[back]<='Z') or(s[back]>='0' and s[back]<='9')))
+                back--;
+            if(((s[front]>='a' and s[front]<='z') or (s[front]>='A' and s[front]<='Z') or (s[front]>='0' and s[front]<='9')) and ((s[back]>='a' and s[back]<='z') or (s[back]>='A' and s[back]<='Z') or (s[back]>='0' and s[back]<='9')))
+           
+            {
+                char c1=tolower(s[front]);
+                char c2=tolower(s[back]);
+                //cout<<c1<<"  "<<c2<<endl;
+                if(c1!=c2)
+                    return false;
+               else 
+               {
+                   front++;
+                   back--;
+               }
+            }
+        }
+        return true;
     }
 };
-
-
-// "A man, a plan, a canal: Panama"
-// "race a car"
-// "   "
-// "   a    "
-// " a    a     "
-// "a a"
-// "a  a"
-// "p  o p   "
