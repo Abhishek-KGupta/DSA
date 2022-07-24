356f0175168011d417421cd9300191541aaadd33
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2022-07-24 10:50:08 +0530

    Time: 25 ms (37.90%), Space: 9.5 MB (98.73%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -5,18 +5,19 @@ public:
         int n=s.size(), j=0;
         for(int i=0;i<n;i++)
         {
-            // if(s[i]!=' ')
-            //     continue;
-            if(s[i]==' ' or i==n-1)
+            if(s[i]==' ')
             {
                 int x;
-                if(i==n-1)
-                    x=i;
-                else 
-                    x=i-1;
+                x=i-1;
                 while(x>j)
                     swap(s[x--],s[j++]);
                 j=i+1;
+            }
+            if(i==n-1)
+            {
+                int x=i;
+                while(x>j)
+                    swap(s[x--], s[j++]);
             }
         }
         return s;
