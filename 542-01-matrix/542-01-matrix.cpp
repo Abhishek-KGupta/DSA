1b616006713c139cb48a52e2ce92f740e7fb67eb
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2022-07-29 12:42:47 +0530

    Time: 104 ms (66.38%), Space: 29.9 MB (73.91%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
+    {
+        int row= mat.size();
+        int col= mat[0].size();
+        queue<pair<int,int>> q;
+        
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(mat[i][j]==0)
+                    q.emplace(i,j);
+                else
+                    mat[i][j]=-1;
+            }
+        }
+        
+        while(!q.empty())
+        {
+            auto [r,c]=q.front();
+            q.pop();
+            for(int i=0; i<4; i++)
+            {
+                int new_r=DIR[i]+r, new_c= DIR[i+1]+c;
+                if(new_r<0 or new_c<0 or new_c>col-1 or new_r>row-1 or mat[new_r][new_c]!=-1)
+                    continue;
+                mat[new_r][new_c] = mat[r][c]+1;
+                q.emplace(new_r, new_c);
+            }
+        }
+        return mat;
+    }
+};
