c558cae478e0f665a68dbc897e23129a7488f6d4
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2022-08-11 20:47:12 +0530

    Time: 45 ms (36.29%), Space: 23.9 MB (94.19%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        long long int tot=1;
-        int z=0;
-        vector<long long int> pre(nums.size(),1);
-        vector<long long int> post(nums.size(),1);
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> arr(n, 1);
+        int right = 1, left = 1;
         
-        for(int i=0;i<nums.size();i++)
+        for(int i = 0; i < n; i++)
         {
-            if(i==0)
-            {
-                pre[i]=nums[i];
-                post[nums.size()-1]=nums[nums.size()-1];
-            }
-            else if(i==nums.size()-1)
-            {
-                pre[i]=pre[i-1]*nums[i];
-                post[0]=post[1]*nums[0];
-            }
-            else 
-            {
-                pre[i]=pre[i-1]*nums[i];
-                post[nums.size()-1-i]=post[nums.size()-i]*nums[nums.size()-1-i ];
-            }
+            arr[i] = left; 
+            left = left * nums[i];
         }
         
-        for(int i=0;i<nums.size();i++)
+        for(int i = n - 1; i >= 0; i--)
         {
-            if(i==0)
-                nums[0]=post[1];
-            else if(i==nums.size()-1)
-                nums[i]=pre[i-1];
-            else
-                nums[i]=pre[i-1]*post[i+1];
+            arr[i] = arr[i] * right;
+            right = right * nums[i];
         }
-            
-        return nums;
-
+        
+        return arr;
     }
 };
