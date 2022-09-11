fb0156a9e26a3fc60ac2a1d42be46dde34ddf1ca
Abhishek-KGupta <83749684+Abhishek-KGupta@users.noreply.github.com>
2022-09-11 19:09:09 +0530

    Time: 60 ms (44.44%), Space: 12.3 MB (82.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    void bfs(int row,int col, vector<vector<int>> &vis, vector<vector<char>> &grid){
-        vis[row][col]=1;
-        queue<pair<int,int>> q;
-        int n = grid.size();
-        int m = grid[0].size();
-        q.push({row,col});
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-	    //for neighbouring elements or cell.
-	    //you could use direct indexes too :)
-            for(int del_row=-1;del_row<=1;del_row++){
-                for(int del_col=-1;del_col<=1;del_col++){
-		    //not to check the diagonal elements.
-                    if((del_row==-1&&del_col==-1) || (del_row==-1&&del_col==1) || (del_row==1&&del_col==-1) || (del_row==1&&del_col==1)){
-                        continue;
-                    }
-                    int nrow=row+del_row;
-                    int ncol=col+del_col;
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                        vis[nrow][ncol]=1;
-                        q.push({nrow,ncol});
-                    }
-                }
-            }
-        }
+    void check(vector<vector<char>>& grid, int i, int j)
+    {
+        if(i < 0 or i >= grid.size() or j < 0 or j >= grid[0].size() or grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        check(grid, i + 1, j);
+        check(grid, i - 1, j);
+        check(grid, i, j + 1);
+        check(grid, i, j - 1);
     }
-    
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size(), cnt=0;
-        vector<vector<int>> vis(n, vector<int>(m,0));
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m;col++){
-                if(!vis[row][col] && grid[row][col]=='1'){
-                    bfs(row,col,vis,grid);
-                    cnt++;
+        int row = grid.size();
+        int col = grid[0].size();
+        int island = 0;
+        
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(grid[i][j] == '1')
+                {
+                    island++;
+                    check(grid, i, j);
                 }
             }
         }
-        return cnt;
+        return island;
     }
-
 };
